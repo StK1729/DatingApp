4f7eff4326fc13edc5af399ff81f9059049191a8
StK1729 <stefan.kraljevic@gmail.com>
2019-08-31 13:53:07 +0200

    added action filter

--- DatingApp-SPA/src/app/app.module.ts
+++ DatingApp-SPA/src/app/app.module.ts
@@ -8,6 +8,7 @@ import { BsDropdownModule, TabsModule, BsDatepickerModule } from 'ngx-bootstrap'
 import { JwtModule } from '@auth0/angular-jwt';
 import { NgxGalleryModule } from 'ngx-gallery';
 import { FileUploadModule } from 'ng2-file-upload';
+import { TimeAgoPipe } from 'time-ago-pipe';
 
 import { AppComponent } from './app.component';
 import { NavComponent } from './nav/nav.component';
@@ -56,7 +57,8 @@ export class CustomHammerConfig extends HammerGestureConfig {
     MemberCardComponent,
     MemberDetailsComponent,
     MemberEditComponent,
-    PhotoEditorComponent
+    PhotoEditorComponent,
+    TimeAgoPipe
   ],
   imports: [
     BrowserModule,
--- DatingApp.api/Startup.cs
+++ DatingApp.api/Startup.cs
@@ -54,7 +54,8 @@ namespace DatingApp.api
                         ValidateIssuer = false,
                         ValidateAudience = false
                     };
-                }); 
+                });
+            services.AddScoped<LogUserActivity>(); 
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
